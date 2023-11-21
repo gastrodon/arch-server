5d3667949b2e7ac489e1339904617498344deb8f
eva <mail@gastrodon.io>
2023-11-21 14:02:01 -0500

    uncommment

--- arch/airootfs/etc/setup/install.sh
+++ arch/airootfs/etc/setup/install.sh
@@ -16,28 +16,26 @@ resize2fs /dev/sda2
 mkdir -p /mnt/{root,boot}
 mount /dev/sda2 /mnt
 
-# cp -r /boot /mnt/boot
+bootstrap arch install
+pacman-key --init
+pacman-key --populate archlinux
+reflector --latest 15 --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Sy
+pacstrap /mnt $(cat /etc/setup/packages)
+genfstab -U /mnt > /mnt/etc/fstab
 
-# bootstrap arch install
-# pacman-key --init
-# pacman-key --populate archlinux
-# reflector --latest 15 --sort rate --save /etc/pacman.d/mirrorlist
-# pacman -Sy
-# pacstrap /mnt $(cat /etc/setup/packages)
-# genfstab -U /mnt > /mnt/etc/fstab
+# copy user info
+cp /etc/{group,gshadow,passwd,shadow,sudoers} /mnt/etc
+mkdir -p /mnt/home/zero
+cp -r /home/zero/. /mnt/home
 
-# # copy user info
-# cp /etc/{group,gshadow,passwd,shadow,sudoers} /mnt/etc
-# mkdir -p /mnt/home/zero
-# cp -r /home/zero/. /mnt/home
+# copy options with overrides, if any
+cp -r /etc/options /mnt/etc
+cp /etc/options/override/* /mnt/etc/options
 
-# # copy options with overrides, if any
-# cp -r /etc/options /mnt/etc
-# cp /etc/options/override/* /mnt/etc/options
+# working in the installed system
+cp /etc/setup/chroot_setup.sh /mnt/chroot_setup.sh
+chmod +x /mnt/chroot_setup.sh
+arch-chroot /mnt /chroot_setup.sh
 
-# # working in the installed system
-# cp /etc/setup/chroot_setup.sh /mnt/chroot_setup.sh
-# chmod +x /mnt/chroot_setup.sh
-# arch-chroot /mnt /chroot_setup.sh
-
-# [ "$(cat /etc/options/REBOOT)" = "true" ] && reboot
+[ "$(cat /etc/options/REBOOT)" = "true" ] && reboot
